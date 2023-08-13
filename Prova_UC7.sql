fe9488e4dd23ae1d75f8c83e1d48861c4e4d0a3c
Vitor <vitorcarvalho.014@gmail.com>
2023-08-13 16:42:27 -0300

    Update Prova_UC7.sql

--- Prova_UC7.sql
+++ Prova_UC7.sql
@@ -6,4 +6,16 @@
  /* Banco de dados da EMPRESAX */ 
 /******************************/
 
-CREATE DATABASE 
+CREATE DATABASE DB_EMPRESAX;
+
+USE DB_EMPRESAX;
+
+CREATE TABLE TLB_DEPARTAMENTO (
+  codDepto SMALLINT PRIMARY KEY AUTO_INCREMENT,
+  nome varchar(50),
+  gerente varchar(50),
+  dataInicioGerente varchar(50));
+
+CREATE TABLE TBL_DEPENDENTE (
+  
+)
